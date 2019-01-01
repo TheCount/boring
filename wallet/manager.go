f398efb98a3f3394cc7d01104bb21f71949ba1f8
Alexander Klauer <Alexander.Klauer@googlemail.com>
2019-01-01 17:44:53 +0100

    Wallet manager: allow creation of wallets

--- wallet/manager.go
+++ wallet/manager.go
@@ -1,6 +1,7 @@
 package wallet
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/TheCount/boring/config"
@@ -14,13 +15,17 @@ type Manager struct {
 
 	// cfg is a pointer to the wallet configuration.
 	cfg *config.WalletConfig
+
+	// openWallets is the set of wallets currently open.
+	openWallets map[string]*Wallet
 }
 
 // NewManager creates a new wallet manager from the specified
 // configuration.
 func NewManager(cfg *config.WalletConfig) *Manager {
 	return &Manager{
-		cfg: cfg,
+		cfg:         cfg,
+		openWallets: make(map[string]*Wallet),
 	}
 }
 
@@ -30,4 +35,27 @@ func (m *Manager) GetWalletNames() (result []string) {
 	defer m.mtx.Unlock()
 	result = append(result, m.cfg.Names...)
 	return
+}
+
+// CreateWallet creates a new wallet with the specified name and passphrase.
+func (m *Manager) CreateWallet(name, password string) error {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+	if m.cfg.HasName(name) {
+		return fmt.Errorf("A wallet named '%s' already exists", name)
+	}
+	m.cfg.AddName(name)
+	wallet, err := NewWallet(m.cfg.WalletsDir, name, password)
+	if err != nil {
+		m.cfg.RemoveName(name)
+		return fmt.Errorf("Unable to create new wallet '%s': %s", name, err)
+	}
+	m.openWallets[name] = wallet
+	if err := m.cfg.SaveNames(); err != nil {
+		return fmt.Errorf(
+			"Wallet '%s' successfully created, but could not save wallet name: %s",
+			name, err,
+		)
+	}
+	return nil
 }
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,2 +1,90 @@
 // Package wallet does all the wallet handling.
 package wallet
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/rand"
+	"errors"
+	"fmt"
+
+	"github.com/tendermint/tendermint/libs/db"
+	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/sha3"
+)
+
+// Prefixes for the various types of data within the wallet.
+const (
+	// prefixPassword is the key used for storing the salted and hashed wallet
+	// password.
+	prefixPassword byte = iota
+
+	// prefixKey is the key used for storing the encrypted master key
+	// of the wallet.
+	prefixKey
+)
+
+const (
+	// passwordHashCost is the cost used for bcrypt hashing of the password.
+	passwordHashCost = 12
+
+	// keyLength is the master key length in bytes.
+	keyLength = 32
+)
+
+// Wallet encapsulates a wallet.
+type Wallet struct {
+	// store is the storage backend of this wallet.
+	store db.DB
+}
+
+// NewWallet creates a new wallet with the specified name in the specified
+// directory. The secret parts of the new wallet will be encrypted using a key
+// derived from the specified password.
+func NewWallet(dir, name, password string) (*Wallet, error) {
+	// Create wallet store.
+	store, err := db.NewGoLevelDB(name, dir)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to create wallet database: %s", err)
+	}
+	success := false
+	defer func() {
+		if !success {
+			store.Close()
+		}
+	}()
+	if store.Iterator(nil, nil).Valid() {
+		return nil, fmt.Errorf("Database '%s/%s' already exists", dir, name)
+	}
+
+	// Hash and salt password and encrypt master key.
+	nonce := make([]byte, keyLength)
+	_, err = rand.Read(nonce)
+	if err != nil {
+		return nil, errors.New("Unable to generate random nonce")
+	}
+	hashedPW, err := bcrypt.GenerateFromPassword(
+		[]byte(password), passwordHashCost,
+	)
+	if err != nil {
+		return nil, errors.New("Unable to hash wallet password")
+	}
+	batch := store.NewBatch()
+	batch.Set([]byte{prefixPassword}, hashedPW)
+	tempKey := sha3.Sum256(bytes.Join([][]byte{
+		[]byte(password), hashedPW,
+	}, nil))
+	cipher, err := aes.NewCipher(tempKey[:])
+	if err != nil {
+		return nil, fmt.Errorf("Unable to create AES-256 cipher: %s", err)
+	}
+	encryptedKey := make([]byte, keyLength)
+	cipher.Encrypt(encryptedKey, nonce)
+	batch.Set([]byte{prefixKey}, encryptedKey)
+	batch.WriteSync()
+
+	success = true
+	return &Wallet{
+		store: store,
+	}, nil
+}
